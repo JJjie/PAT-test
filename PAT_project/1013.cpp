bd87ec5f536fe666b94044a88feacdae3cbd5194
胡文杰 <hwj950504@163.com>
2017-01-07 16:04:56 +0800

    1013 校验通过版本

--- PAT_project/1013.cpp
+++ PAT_project/1013.cpp
@@ -5,8 +5,59 @@
 #include "pat.h"
 #include <iostream>
 using namespace std;
+int N;
+
+void DFS(int **cities, int index, bool *visited);
 
 int pat_1013(){
+    int M, K;
+    cin >> N >> M >> K;
+    int **cities = new int *[N];
+    bool *visited = new bool [N];
+    for (int i = 0; i < N; i++) {
+        cities[i] = new int [N];
+        visited[i] = false;
+        for (int j = 0; j < N; j++) {
+            cities[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < M; i++) {
+        int i1, i2;
+        cin >> i1 >> i2;
+        i1 = i1-1, i2 = i2-1;
+        cities[i1][i2] = 1;
+        cities[i2][i1] = 1;
+    }
+    for (int i = 0; i < K; i++) {
+        int target, num = 0;
+        for (int j = 0; j < N; j++) {//每一次假设占领前将visited初始化
+            visited[j] = false;
+        }
+        cin >> target;
+        visited[target-1] = true;
+        for (int j = 0; j < N; j++) {
+            if (!visited[j]){
+                DFS(cities, j, visited);
+                num ++;
+            }
+        }
+        cout << num-1 << endl;
+    }
+
+//    释放内存
+    for (int i = 0; i < N; i++) {
+        delete[] cities[i];
+    }
+    delete[] visited;
 
     return 0;
+}
+
+void DFS(int **cities, int index, bool *visited){
+    visited[index] = true;
+    for (int i = 0; i < N; ++i) {
+        if (!visited[i] && cities[index][i] == 1){
+            DFS(cities, i, visited);
+        }
+    }
 }
