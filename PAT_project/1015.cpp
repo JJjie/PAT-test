19e040f883f9b292b0ebeecb99ab6b5cb20e3452
胡文杰 <hwj950504@163.com>
2017-01-09 13:36:13 +0800

    1015 校验通过版本

--- PAT_project/1015.cpp
+++ PAT_project/1015.cpp
@@ -4,8 +4,49 @@
 
 #include "pat.h"
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int pat_1015(){
+bool isprime (int num);
+int reverse (int num,int radix);
 
+int pat_1015(){
+    int prime,radix;
+    cin >> prime;
+    while (prime > 0){
+        cin >> radix;
+        if (isprime(prime)){
+            int num = reverse(prime,radix);
+            if (isprime(num)){
+                cout << "Yes" << endl;
+            } else{
+                cout << "No" << endl;
+            }
+        } else{
+            cout << "No" << endl;
+        }
+        cin >> prime;
+    }
+    return 0;
+}
+
+bool isprime(int num){
+    if (num > 1){
+        int sq = (int)sqrt((double)num);
+        int i;
+        for (i = 2; i <= sq; i++) {
+            if (num % i == 0) break;
+        }
+        if (i > sq) return true;
+    }
+    return false;
+}
+//基于基数的逆，真值为十进制
+int reverse(int num, int radix){
+    int renum = 0;
+    while (num > 0){
+        renum = renum * radix + num % radix;
+        num = num/radix;
+    }
+    return renum;
 }
