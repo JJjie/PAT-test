84af315731021f45359763678bc9266bef69f2ac
胡文杰 <hwj950504@163.com>
2017-01-24 21:40:29 +0800

    1057 校验通过

--- PAT_project/1057.cpp
+++ PAT_project/1057.cpp
@@ -4,43 +4,62 @@
 
 #include "pat.h"
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <stack>
+//#define lowbit(i) ((i) & (-i))
 using namespace std;
+int c[100001];
+stack<int> v; //作为全局变量，减少调参的延迟
 
-void peekMedian(vector<int> v){
-    int len = v.size();
-    vector<int> vt(len);
-    for (int i = 0; i < len; i++) {
-        vt[i] = v[i];
+int lowbit(int x){ //树状数组，返回'C[n]=A[x-2^k+1]+...+A[x]'中2^k的值
+    return (x)&(-x);//按位与
+}
+void update(int x, int v){
+    for (int i = x; i < 100001 ; i += lowbit(i)) {
+        c[i] += v;
     }
-    int index;
-    if (len % 2 == 0) index = len / 2 - 1;
-    else index = (len+1)/2 - 1;
-    sort(vt.begin(), vt.end());
-    printf("%d\n",vt[index]);
+}
+int getsum(int x){
+    int sum = 0;
+    for (int i = x; i >= 1 ; i -= lowbit(i)) {
+        sum += c[i];
+    }
+    return sum;
+}
+
+void peekMedian(){
+    int left = 1, right = 100001, mid, k = (v.size() + 1) / 2;
+    while(left < right) {
+        mid = (left + right) / 2;
+        if(getsum(mid) >= k)
+            right = mid;
+        else
+            left = mid + 1;
+    }
+    printf("%d\n", left);
+
 }
 
 int pat_1057(){
     int n,temp;
     char str[11];
     scanf("%d",&n);
-    vector<int> v;
     for (int i = 0; i < n; i++) {
         scanf("%s",str);
         if (str[1] == 'u'){
-            cin >> temp;
-            v.push_back(temp);
+            scanf("%d",&temp);
+            v.push(temp);
+            update(temp,1);
         } else if (str[1] == 'o'){
             if (!v.empty()){
-                printf("%d\n",v.back());
-                v.pop_back();
+                update(v.top(),-1);
+                printf("%d\n",v.top());
+                v.pop();
             } else{
                 printf("Invalid\n");
             }
         } else {
             if (!v.empty()){
-                peekMedian(v);
+                peekMedian();
             } else{
                 printf("Invalid\n");
             }
